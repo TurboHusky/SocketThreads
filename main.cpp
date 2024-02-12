d5b45349b12adac2eaa04156f9ffcfbd43d0c2e6
Ben P <ben.passman@outlook.com>
2024-02-12 08:30:02 +0000

    Timer test with Poco threads

--- main.cpp
+++ main.cpp
@@ -1,119 +1,74 @@
-#include <sys/socket.h>
-#include <netinet/in.h> // Input struct for sockets
-#include <arpa/inet.h>  // Helper functions for ip conversion
-#include <thread>
+#include <memory>
 #include <iostream>
 
-int client_app(sockaddr_in server_addr)
+#include <Poco/Timer.h>
+#include <Poco/Thread.h>
+
+class HeartbeatTimer
 {
-    int client;
-    client = socket(AF_INET, SOCK_STREAM, 0);
-    if (client < 0)
+public:
+    HeartbeatTimer(const std::string connectionID, const int timeout_ms) : _connectionID(connectionID), _timeout_ms(timeout_ms), _rxTimeElapsed(0), _txTimeElapsed(0), _alive(true)
     {
-        std::cout << "Client: Failed to open socket." << std::endl;
-        return 1;
+        _timer = std::make_unique<Poco::Timer>(250, 250); // TODO: Remove magic numbers
+        _timer->start(Poco::TimerCallback<HeartbeatTimer>(*this, &HeartbeatTimer::OnTimer));
     }
-    std::cout << "Client: Connecting...\n";
-    if (connect(client, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+
+    bool IsAlive()
     {
-        std::cout << "Client: Failed to connect - " << strerror(errno) << std::endl;
-        return 1;
+        return _alive;
     }
-    char send_buffer[65536]{"Client message."};
-    std::cout << "Client: Sending...\n";
-    send(client, send_buffer, strlen(send_buffer), 0);
-    int reader;
-    char buffer[1024]{0};
-    reader = recv(client, &buffer, 1024, MSG_WAITFORONE);
-    std::cout << "Client: Read " << reader << " bytes: \"" << buffer << "\"\n";
 
-    std::cout << "Client: Cleaning up." << std::endl;
-    shutdown(client, SHUT_RDWR);
-
-    return 0;
-}
-
-int server_app(sockaddr_in server_addr)
-{
-    int server;
-    server = socket(AF_INET, SOCK_STREAM, 0);
-    if (server < 0)
+    void ResetHeartbeat()
     {
-        std::cout << "Server: Failed to open socket." << std::endl;
-        return 1;
+        std::cout << _connectionID << " - Resetting heartbeat" << '\n';
+        _rxTimeElapsed = 0;
     }
-    int opted{1};
-    if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opted, sizeof(opted)))
-    { // Set option at socket level to let bind reuse local addresses.
-        std::cout << "Server: Failed to set socket options - " << strerror(errno) << std::endl;
-        return 1;
-    }
-    // Bind
-    std::cout << "Server: Binding...\n";
-    if (bind(server, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+
+    void OnTimer(Poco::Timer& timer)
     {
-        std::cout << "Sever: Failed to bind to address - " << strerror(errno) << "\n";
-        return 1;
-    }
-    // Listen
-    std::cout << "Server: Listening...\n";
-    if (listen(server, 3) < 0)
-    {
-        std::cout << "Server: Failed to start listening - " << strerror(errno) << std::endl;
-        return 1;
-    }
+        auto timeInterval = timer.getPeriodicInterval();
+        _rxTimeElapsed += timeInterval;
+        _txTimeElapsed += timeInterval;
 
-    // Accept
-    int conn;
-    socklen_t server_length = sizeof(server_addr);
-    std::cout << "Server: Accept connection...\n";
-    conn = accept(server, (struct sockaddr *)&server_addr, &server_length);
-    if (conn < 0)
-    {
-        std::cout << "Server: Failed to accept connection." << std::endl;
-        return 1;
+        if (_rxTimeElapsed > (_timeout_ms << 1))
+        {
+            std::cout << _connectionID << " Heartbeat timed out after " << _rxTimeElapsed << '\n';
+            // OnHeartbeatTimeout()
+            timer.restart(0); // Stop the timer
+            _alive = false;
+        }
+        else if (_txTimeElapsed > _timeout_ms)
+        {
+            std::cout << _connectionID << " Send heartbeat after " << _txTimeElapsed << '\n';
+            // SendHeartbeat()
+            _txTimeElapsed = 0;
+        }
     }
 
-    int reader;
-    char buffer[1024]{0};
-    reader = recv(conn, &buffer, 1024, MSG_WAITFORONE);
-    // reader = read(conn, &buffer, 1024);
-    std::cout << "Server: Read " << reader << " bytes: \"" << buffer << "\"\n";
-    char msg[]{"Server response."};
-    send(conn, msg, strlen(msg), 0);
-    std::cout << "Server: Sent response to client.\n";
-
-    std::cout << "Server: Cleaning up.\n";
-    shutdown(conn, SHUT_RDWR);
-    shutdown(server, SHUT_RDWR);
-
-    return 0;
-}
+private:
+    std::unique_ptr<Poco::Timer> _timer;
+    const std::string _connectionID;
+    const int _timeout_ms;
+    int _rxTimeElapsed;
+    int _txTimeElapsed;
+    bool _alive;
+};
 
 int main(int argc, char const *argv[])
 {
     (void)argc;
     (void)argv; // Ignore unused input variables
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(9977); // Set port to network order
+    std::cout << "Starting Heartbeat timers\n";
+    auto handler = std::make_unique<HeartbeatTimer>("fgaa45-7556b2", 1100);
+    auto handler2 = std::make_unique<HeartbeatTimer>("baa564-887da9", 2000);
 
-    std::thread t1(server_app, server_addr);
-
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr.s_addr) <= 0)
-    {
-        std::cout << "Failed to convert IP to binary format." << std::endl;
-        return 1;
-    }
-    // server_addr.sin_addr.s_addr = INADDR_LOOPBACK;
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    std::thread t2(client_app, server_addr);
-    t1.join();
-    t2.join();
+    Poco::Thread::sleep(1000);
+    handler->ResetHeartbeat();
+    Poco::Thread::sleep(1000);
+    handler->ResetHeartbeat();
+    handler2->ResetHeartbeat();
+    Poco::Thread::sleep(5000);
 
     return 0;
 }
