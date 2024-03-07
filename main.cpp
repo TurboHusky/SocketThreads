66a7d7ce1881839933a84dea81747058c696ba5a
Ben P <ben.passman@outlook.com>
2024-03-07 07:07:28 +0000

    Linux UDP

--- main.cpp
+++ main.cpp
@@ -7,24 +7,19 @@
 int client_app(sockaddr_in server_addr)
 {
     int client;
-    client = socket(AF_INET, SOCK_STREAM, 0);
+    client = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (client < 0)
     {
         std::cout << "Client: Failed to open socket." << std::endl;
         return 1;
     }
-    std::cout << "Client: Connecting...\n";
-    if (connect(client, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        std::cout << "Client: Failed to connect - " << strerror(errno) << std::endl;
-        return 1;
-    }
+    socklen_t server_len = sizeof(server_addr);
     char send_buffer[65536]{"Client message."};
     std::cout << "Client: Sending...\n";
-    send(client, send_buffer, strlen(send_buffer), 0);
+    sendto(client, send_buffer, strlen(send_buffer), 0, (struct sockaddr *) &server_addr, server_len);
     int reader;
     char buffer[1024]{0};
-    reader = recv(client, &buffer, 1024, MSG_WAITFORONE);
+    reader = recvfrom(client, &buffer, 1024, 0, (struct sockaddr *) &server_addr, &server_len);
     std::cout << "Client: Read " << reader << " bytes: \"" << buffer << "\"\n";
 
     std::cout << "Client: Cleaning up." << std::endl;
@@ -36,7 +31,7 @@ int client_app(sockaddr_in server_addr)
 int server_app(sockaddr_in server_addr)
 {
     int server;
-    server = socket(AF_INET, SOCK_STREAM, 0);
+    server = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (server < 0)
     {
         std::cout << "Server: Failed to open socket." << std::endl;
@@ -55,36 +50,19 @@ int server_app(sockaddr_in server_addr)
         std::cout << "Sever: Failed to bind to address - " << strerror(errno) << "\n";
         return 1;
     }
-    // Listen
-    std::cout << "Server: Listening...\n";
-    if (listen(server, 3) < 0)
-    {
-        std::cout << "Server: Failed to start listening - " << strerror(errno) << std::endl;
-        return 1;
-    }
-
-    // Accept
-    int conn;
-    socklen_t server_length = sizeof(server_addr);
-    std::cout << "Server: Accept connection...\n";
-    conn = accept(server, (struct sockaddr *)&server_addr, &server_length);
-    if (conn < 0)
-    {
-        std::cout << "Server: Failed to accept connection." << std::endl;
-        return 1;
-    }
 
     int reader;
     char buffer[1024]{0};
-    reader = recv(conn, &buffer, 1024, MSG_WAITFORONE);
+    struct sockaddr_in client;
+    socklen_t client_len = sizeof(client);
+    reader = recvfrom(server, buffer, 1024, 0, (struct sockaddr *) &client, &client_len);
     // reader = read(conn, &buffer, 1024);
     std::cout << "Server: Read " << reader << " bytes: \"" << buffer << "\"\n";
     char msg[]{"Server response."};
-    send(conn, msg, strlen(msg), 0);
+    sendto(server, msg, strlen(msg), 0, (struct sockaddr *) &client, client_len);
     std::cout << "Server: Sent response to client.\n";
 
     std::cout << "Server: Cleaning up.\n";
-    shutdown(conn, SHUT_RDWR);
     shutdown(server, SHUT_RDWR);
 
     return 0;
